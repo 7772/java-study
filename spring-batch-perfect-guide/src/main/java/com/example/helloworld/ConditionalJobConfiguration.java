42b77b45d33bcac4b9ff90cc0c9b7f6edae16742
박현도 <hyundo@wemakeprice.com>
2021-07-18 10:29:34 +0900

    Add ConditionalJob

---
+++ spring-batch-perfect-guide/src/main/java/com/example/helloworld/ConditionalJobConfiguration.java
@@ -0,0 +1,79 @@
+package com.example.helloworld;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory; import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class ConditionalJobConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Tasklet passTasklet() {
+        return (contribution, chunkContext) -> {
+//            throw new RuntimeException("This is failure !");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public Tasklet successTasklet() {
+        return (contribution, context) -> {
+            System.out.println("Success !");
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public Tasklet failTasklet() {
+        return (contribution, context) -> {
+            System.out.println("Failure !");
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public Job conditionalJob() {
+        return jobBuilderFactory.get("conditionalJob")
+            .start(firstStep())
+            .on("FAILED").to(failureStep())
+            .from(firstStep()).on("*").to(successStep())
+            .end()
+            .build();
+    }
+
+    @Bean
+    public Step firstStep() {
+        return stepBuilderFactory.get("firstStep")
+            .tasklet(passTasklet())
+            .build();
+    }
+
+    @Bean
+    public Step successStep() {
+        return stepBuilderFactory.get("successStep")
+            .tasklet(successTasklet())
+            .build();
+    }
+
+    @Bean
+    public Step failureStep() {
+        return stepBuilderFactory.get("failureStep")
+            .tasklet(failTasklet())
+            .build();
+    }
+}
