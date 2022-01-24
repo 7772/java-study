d360226bc32c34acd59d92ba78525965327d4a2f
박현도 <hyundo@wemakeprice.com>
2022-01-24 18:55:51 +0900

    SpringStudyApplication

--- SpringStudy/src/main/java/com/example/springstudy/lockstratedgy/UserRepository.java
+++ SpringStudy/src/main/java/com/example/springstudy/lockstratedgy/UserRepository.java
@@ -1,2 +1,16 @@
-package com.example.springstudy.lockstratedgy;public class UserRepository {
+package com.example.springstudy.lockstratedgy;
+
+import java.util.Optional;
+
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("SELECT u FROM User u WHERE u.id = ?1")
+    Optional<User> findById(Long id);
 }
